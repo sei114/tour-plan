38aa2f280afa826c2bb7d57e3c4f6e4e747c7126
sei114 <1147017@gmail.com>
2021-07-24 12:55:22 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
-const swiper = new Swiper(".swiper-container", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
 
   // Мои параметры
   effect: "fade",
   speed: 800,
 });
-
 /* Усложнённое задание */
 // window.addEventListener("keydown", (arrowleft) => console.log("Вы нажали на кнопку Стрелка влево"));
 // window.addEventListener("keydown", (arrowleft) => swiper.navigation.prevEl);
 // window.addEventListener("keydown", (arrowright) => swiper.navigation.nextEl);
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+
+  // Мои параметры
+  speed: 800,
+});
